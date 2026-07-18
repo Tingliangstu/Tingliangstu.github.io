88260c41183c249fe70e0c2031b94d15c71226fb
Liangting <13215638486@163.com>
2026-07-18 18:20:48 +0800

    Update Scholar publications July 2026

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const PAGE = document.body.dataset.page || "home";
 const BASE_PATH = document.body.dataset.basePath || ".";
-const DATA_VERSION = "20260705-scholar-citations";
+const DATA_VERSION = "20260718-scholar-citations";
 
 function withBase(path) {
   if (!path) {
