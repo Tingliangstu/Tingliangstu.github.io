a458c343b01a98b38d748931908f028aee488f07
Liangting <13215638486@163.com>
2026-08-28 11:12:34 +0800

    Sync Google Scholar publications and citations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const PAGE = document.body.dataset.page || "home";
 const BASE_PATH = document.body.dataset.basePath || ".";
-const DATA_VERSION = "20260809-mdtrace";
+const DATA_VERSION = "20260828-scholar-sync";
 
 function withBase(path) {
   if (!path) {
